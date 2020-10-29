d1b8f1126f497da65e05fba980b1abbeeebd9277
swj1265 <swj1265@naver.com>
2020-10-29 15:13:12 +0900

    Test Commit

--- app/src/main/java/com/example/a2020_dm_term/DMApp/DB/PlanDB.java
+++ app/src/main/java/com/example/a2020_dm_term/DMApp/DB/PlanDB.java
@@ -9,7 +9,7 @@ public class PlanDB {
         public static final String CONTENTS = "CONTENTS";
         public static final String STARTDATE = "STARTDATE";
         public static final String ENDDATE = "ENDDATE";
-        public static final String _TABLENAME1 = "ingredients";
+        public static final String _TABLENAME1 = "PLAN";
         public static final String _CREATE1 ="create table if not exists "+_TABLENAME1+"("
                 +_ID+" integer primary key autoincrement, "
                 +NAME+" text not null , "

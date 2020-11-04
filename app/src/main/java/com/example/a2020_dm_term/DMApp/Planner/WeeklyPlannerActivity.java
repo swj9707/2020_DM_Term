a03fd56103cde79e9687c7a70096a218def04a44
amugae568 <anjffhgkwl00@gmail.com>
2020-11-04 23:04:17 +0900

    2020.11.04 등록버튼 추가 - 등록버튼을 누르면 다이얼로그가 뜨고 시간 설정, 제목 설정을 할 수 았움 - 제목 설정을 하면 해당 이름의 버튼의 작업블록이 등록 버튼 우측으로 생성된다.
    추후 수정할 사항
    다이얼로그에 timepicker를 사용해서 시간을 설정하도록 하였는데 작업블록은 테이블에 끌어다 놓음으로 자동으로 시간배치를 할 것이기 때문에 numberpicker혹은 edittext를 이용해서 기간만 받도록 할 예정임.

--- app/src/main/java/com/example/a2020_dm_term/DMApp/Planner/WeeklyPlannerActivity.java
+++ app/src/main/java/com/example/a2020_dm_term/DMApp/Planner/WeeklyPlannerActivity.java
@@ -1,4 +1,92 @@
 package com.example.a2020_dm_term.DMApp.Planner;
 
-public class WeeklyPlannerActivity {
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.a2020_dm_term.R;
+
+import java.util.ArrayList;
+
+public class WeeklyPlannerActivity extends AppCompatActivity {
+    ArrayList<TaskBlock> taskBlockArrayList = new ArrayList<TaskBlock>();
+    Context context;
+    LinearLayout task_layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weeklyplan);
+        context = this;
+        task_layout = (LinearLayout) findViewById(R.id.task_layout);
+    }
+
+    class TaskBlock {
+        int period;
+        String title;
+        int hour;
+        int minute;
+
+        void TaskBlock() {
+        }
+    }
+
+    int dp_to_px(int dp) {
+        float mScale = getResources().getDisplayMetrics().density;
+        int px = (int) (dp * mScale);
+        return px;
+    }
+
+    Button make_button(String str) {
+        Button newBtn = new Button(this.context);
+        newBtn.setText(str);
+        newBtn.setWidth(dp_to_px(140));
+        newBtn.setHeight(dp_to_px(140));
+
+        return newBtn;
+    }
+
+    public void addBtnOnClick(View view) {
+        System.out.println("add btn onclick");
+        final Dialog dialog = new Dialog(this);
+        final TaskBlock newTask = new TaskBlock();
+        dialog.setContentView(R.layout.add_new_task);
+
+        Button confirm = dialog.findViewById(R.id.add_confirm);
+        Button cancel = dialog.findViewById(R.id.add_cancel);
+        final EditText task_name = dialog.findViewById(R.id.task_title);
+        final TimePicker timePicker = dialog.findViewById(R.id.timePicker);
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("confirm button is clicked.");
+                newTask.hour = timePicker.getHour();
+                newTask.minute = timePicker.getMinute();
+                newTask.title = task_name.getText().toString();
+                taskBlockArrayList.add(newTask);
+
+                int lastIndex = taskBlockArrayList.size() - 1;
+                System.out.printf("hour = %d, minute = %d\n",
+                        taskBlockArrayList.get(lastIndex).hour,
+                        taskBlockArrayList.get(lastIndex).minute);
+                dialog.dismiss();
+
+                task_layout.addView(make_button(newTask.title));
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("cancel button is clicked.");
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
 }
+

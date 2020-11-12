45a6e988c75af93d1bba74a594647775227605ac
Sierra <swj1265@naver.com>
2020-11-12 12:17:49 +0900

    MainActivity 추가

--- app/src/main/java/com/example/a2020_dm_term/DMApp/Planner/MainActivity.java
+++ app/src/main/java/com/example/a2020_dm_term/DMApp/Planner/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.a2020_dm_term.DMApp.Planner;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.a2020_dm_term.R;
+
 /*
 실제 플래너가 실행되는 공간.
 * */
@@ -17,11 +19,14 @@ public class MainActivity extends AppCompatActivity {
     Button weeklyPlanButton = (Button)findViewById(R.id.weeklyPlanButton);
     Button monthlyPlanButton = (Button)findViewById(R.id.monthlyPlanButton);
     Button restrictModeButton = (Button)findViewById(R.id.restrictModeButton);
+    Intent restrictIntent;
+    Intent weeklyIntent;
 
 
     class weeklyButtonListener implements View.OnClickListener{
         public void onClick(View v){
             //주간 계획으로 넘어감
+            startActivity(weeklyIntent);
         }
     }
     class monthlyButtonListener implements View.OnClickListener{
@@ -32,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     class restrictButtonListener implements View.OnClickListener{
         public void onClick(View v){
             //제한모드로 넘어감
+            startActivity(restrictIntent);
         }
     }
 
@@ -40,16 +46,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        restrictIntent = new Intent(this, RestrictActivity.class);
+        weeklyIntent = new Intent(this, WeeklyPlannerActivity.class);
         //System.out.println("Test Commit WJ");
         context = this;
+
         weeklyButtonListener wButtonListener = new weeklyButtonListener();
         monthlyButtonListener mButtonListener = new monthlyButtonListener();
         restrictButtonListener rButtonListener = new restrictButtonListener();
+
         weeklyPlanButton.setOnClickListener(wButtonListener);
         monthlyPlanButton.setOnClickListener(mButtonListener);
         restrictModeButton.setOnClickListener(rButtonListener);
-
-
 
     }
 }

4aa2ff7a8c7f272c586cf88b6484f80920847c1e
amugae568 <anjffhgkwl00@gmail.com>
2020-11-16 23:54:30 +0900

    2020.11.16. 수정사항 일주일 시간표 추가 - 일주일 시간표(25*8)등록. 인덱스 번호(시간)와 요일 행도 포함
    드래그 앤 드롭 기능 추가 2
    - 생성한 작업 블럭을 표에 드롭하면 해당 시간만큼 셀의 행을 늘린다.
    - 드래그 드롭의 액션시 블럭의 텍스트를 가져오는 것에서 TaskBlock을 전해줘서 작업 블럭의 내용(시간, 제목 등)을 전달한다.
    
    기타 변경 사항
    - 시간표 레이아웃을 테이블 레이아웃에서 상대 레이아웃으로 변경
    - 작업 추가 팝업창에서 TimePicker를 삭제하고 NumberPicker로 변경
    
    추후 수정할 사항
    - 적절한 위치에 드롭하지 않을 때, 드래그한 뷰의 그림자가 부자연스럽게 돌아감
    - 기존에는 작업 블럭을 생성하면 해당 내용을 TaskBlock 클래스에 저장하고 ArrayList에 추가하였으나 CustomButton을 만들어 버튼에 포함시켰음. 경과를 지켜본 다음 문제 없을 시 추후 삭제할 예정

--- app/src/main/java/com/example/a2020_dm_term/DMApp/Planner/WeeklyPlannerActivity.java
+++ app/src/main/java/com/example/a2020_dm_term/DMApp/Planner/WeeklyPlannerActivity.java
@@ -2,8 +2,6 @@ package com.example.a2020_dm_term.DMApp.Planner;
 
 
 import android.app.Dialog;
-import android.content.ClipData;
-import android.content.ClipDescription;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -12,9 +10,11 @@ import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.DragEvent;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import com.example.a2020_dm_term.R;
 import org.w3c.dom.Text;
 
@@ -24,7 +24,8 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
     ArrayList<TaskBlock> taskBlockArrayList = new ArrayList<TaskBlock>();
     Context context;
     LinearLayout task_layout;
-    TextView dropTest;
+    GridLayout timeGrid;
+    RelativeLayout timeTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,35 +34,99 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
 
         context = this;
         task_layout = (LinearLayout) findViewById(R.id.task_layout);
-        dropTest = (TextView) findViewById(R.id.drop_test);
-        dropTest.setOnDragListener(new myOnDragListener());
+        //timeGrid = (GridLayout) findViewById(R.id.time_grid);
+        timeTable = findViewById(R.id.time_table);
+        timeTable.setBackgroundColor(Color.BLACK);
+
+        for (int hour = 1; hour <= 24; hour++) {
+            TextView rowIndex = new TextView(this);
+            rowIndex.setText(Integer.toString(hour - 1));
+            rowIndex.setGravity(Gravity.CENTER);
+            rowIndex.setBackgroundColor(Color.WHITE);
+            rowIndex.setId(hour * 10);
+
+            RelativeLayout.LayoutParams idx_params = new RelativeLayout.LayoutParams(dp2px(20), dp2px(100));
+            idx_params.setMargins(0, dp2px(1), 0, 0);
+            if (hour == 1)
+                idx_params.addRule(RelativeLayout.BELOW, R.id.blank);
+            else
+                idx_params.addRule(RelativeLayout.BELOW, (hour - 1) * 10);
+
+            rowIndex.setLayoutParams(new RelativeLayout.LayoutParams(idx_params));//w, h
+            timeTable.addView(rowIndex);
+
+            for (int day = 1; day <= 7; day++) {
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(dp2px(100), dp2px(100));
+                params.addRule(RelativeLayout.RIGHT_OF, hour * 10);
+                params.addRule(RelativeLayout.ALIGN_TOP, hour * 10);
+                TextView cell = new TextView(this);
+                params.setMargins(dp2px(1) * day + dp2px(100) * (day - 1), 0, 0, dp2px(1));
+
+                cell.setId(hour * 10 + day);
+                cell.setOnDragListener(new myOnDragListener());
+                cell.setGravity(Gravity.CENTER);
+                cell.setBackgroundColor(Color.WHITE);
+                cell.setLayoutParams(params);
+                timeTable.addView(cell);
+            }
+        }
+
+        /*for (int hour = 0; hour < 24; hour++) {
+            TableRow tableRow = new TableRow(this);
+            tableRow.setMinimumHeight(dp2px(100));
+            tableRow.setBackgroundColor(Color.BLACK);
+
+            TableRow.LayoutParams params = new TableRow.LayoutParams();
+            params.height = TableRow.LayoutParams.MATCH_PARENT;
+
+            TextView rowIndex = new TextView(this);
+            rowIndex.setText(Integer.toString(hour));
+            rowIndex.setGravity(Gravity.CENTER);
+            rowIndex.setBackgroundColor(Color.WHITE);
+
+            rowIndex.setLayoutParams(params);
+            tableRow.addView(rowIndex);
+
+            for (int day = 0; day < 7; day++) {
+                TextView td = new TextView(this);
+                params.setMargins(1, 1, 1, 1);
+
+                td.setId(hour * 10 + day);
+                td.setOnDragListener(new myOnDragListener());
+                td.setWidth(dp2px(100));
+                td.setGravity(Gravity.CENTER);
+                td.setBackgroundColor(Color.WHITE);
+                td.setLayoutParams(params);
+                tableRow.addView(td);
+            }
+            timeTable.addView(tableRow);
+        }*/
     }
 
     public void addBtnOnClick(View view) {//추가 버튼 눌렀을 시 동작
         System.out.println("add btn onclick");
 
         final Dialog dialog = new Dialog(this);
-        final TaskBlock newTask = new TaskBlock();//새로 생성하는 작업 내용을 담는 클래스
+        final TaskBlock task = new TaskBlock();//새로 생성하는 작업 내용을 담는 클래스
         dialog.setContentView(R.layout.add_new_task);
 
         Button confirm = dialog.findViewById(R.id.add_confirm);
         Button cancel = dialog.findViewById(R.id.add_cancel);
         final EditText task_name = dialog.findViewById(R.id.task_title);
-        final TimePicker timePicker = dialog.findViewById(R.id.timePicker);//삭제할 예정
+        final NumberPicker numberPicker = dialog.findViewById(R.id.numberPicker);
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(24);
 
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 System.out.println("confirm button is clicked.");
-                newTask.hour = timePicker.getHour();//삭제할 예정
-                newTask.minute = timePicker.getMinute();//삭제할 예정
-                newTask.title = task_name.getText().toString();
-                taskBlockArrayList.add(newTask);
-
-                int lastIndex = taskBlockArrayList.size() - 1;
+                task.title = task_name.getText().toString();
+                task.period = numberPicker.getValue();
+                taskBlockArrayList.add(task);
                 dialog.dismiss();
 
-                task_layout.addView(mkBtn(newTask.title));
+                task_layout.addView(mkBtn(task));
             }
         });
 
@@ -76,9 +141,10 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
         dialog.show();
     }
 
-    public Button mkBtn(String str) {
-        Button newBtn = new Button(this.context);
-        newBtn.setText(str);
+    public CustomButton mkBtn(TaskBlock task) {
+        CustomButton newBtn = new CustomButton(this.context, task);
+        newBtn.setText(task.title + "\n" + Integer.toString(task.period));
+        //newBtn.setText(Html.fromHtml(task.title + "<br><small>" + Integer.toString(task.period) + "<small>"));
         newBtn.setWidth(dp2px(140));
         newBtn.setHeight(dp2px(140));
         setDrag(newBtn);
@@ -94,9 +160,9 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
                 ClipData.Item item = new ClipData.Item((CharSequence) view.getTag());
                 ClipData dragData = new ClipData((CharSequence) view.getTag(), new String[]{ClipDescription.MIMETYPE_TEXT_PLAIN}, item);
                  */
-                Button tmp = (Button)view2;
+                CustomButton tmp = (CustomButton) view2;
                 View.DragShadowBuilder shadow = new myDragShadowBuilder(view2);
-                view2.startDragAndDrop(null, shadow, tmp.getText(), 0);//(전달할 데이터, 그림자 생성자, 전달할 데이터(로컬), 플래그
+                view2.startDragAndDrop(null, shadow, tmp.task, 0);//(전달할 데이터, 그림자 생성자, 전달할 데이터(로컬), 플래그
                 //local state = When dispatching drag events to views in the same activity this object will be available through getLocalState().
                 return true;
             }
@@ -106,32 +172,48 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
     class myOnDragListener implements View.OnDragListener {
         @Override
         public boolean onDrag(View v, DragEvent event) {
+            TextView textView = (TextView) v;
             switch (event.getAction()) {
                 case DragEvent.ACTION_DRAG_STARTED:
-                    System.out.println(getResources().getResourceName(v.getId()) + " ACTION_DRAG_STARTED");
+                    //System.out.println(v.getId() + " ACTION_DRAG_STARTED");
                     break;
                 case DragEvent.ACTION_DRAG_ENTERED:
-                    System.out.println(getResources().getResourceName(v.getId()) + " ACTION_DRAG_ENTERED");
+                    //System.out.println(v.getId() + " ACTION_DRAG_ENTERED");
                     break;
                 case DragEvent.ACTION_DRAG_EXITED:
-                    System.out.println(getResources().getResourceName(v.getId()) + " ACTION_DRAG_EXITED");
+                    //System.out.println(v.getId() + " ACTION_DRAG_EXITED");
                     break;
                 case DragEvent.ACTION_DROP:
-                    String tmp = (String)event.getLocalState();
-                    dropTest.setText(tmp);
-                    System.out.println(getResources().getResourceName(v.getId()) + " ACTION_DROP");
+                    TaskBlock task = (TaskBlock) event.getLocalState();
+                    int period = task.period;
+                    int hour = textView.getId() / 10;
+                    textView.setText(task.title);
+                    System.out.println(v.getId() + " ACTION_DROP period: " + period + " hour: " + (hour - 1));
+
+                    if (period == 1)
+                        break;
+
+                    for (int i = 1; i <= period - 1; i++)
+                        timeTable.removeView(findViewById(v.getId() + i * 10));
+                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) textView.getLayoutParams();
+                    params.height = period * textView.getHeight() + (period - 1) * dp2px(1);
+                    textView.setLayoutParams(params);
                     break;
                 case DragEvent.ACTION_DRAG_ENDED:
-                    System.out.println(getResources().getResourceName(v.getId()) + "ACTION_DRAG_ENDED");
+                    //System.out.println(v.getId() + "ACTION_DRAG_ENDED");
                     break;
             }
             return true;
         }
     }
 
+    public void mergeCells(int period, int id) {
+
+    }
+
     public int dp2px(int dp) {
-        float mScale = getResources().getDisplayMetrics().density;
-        int px = (int) (dp * mScale);
+        float scale = getResources().getDisplayMetrics().density;
+        int px = (int) (dp * scale + 0.5f);
         return px;
     }
 }
@@ -161,5 +243,14 @@ class myDragShadowBuilder extends View.DragShadowBuilder {//드래그 시 이미
 
     public void onDrawShadow(Canvas canvas) {
         shadow.draw(canvas);
+    }
+}
+
+class CustomButton extends AppCompatButton {
+    TaskBlock task;
+
+    CustomButton(Context context, TaskBlock task) {
+        super(context);
+        this.task = task;
     }
 }

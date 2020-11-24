a178250e034a8bb4499ae3579652cc04b451b98c
Sierra <swj1265@naver.com>
2020-11-25 02:47:43 +0900

    20201125 타이머 화면 꺼졌을때도 동작하게 완료

--- app/src/main/java/com/example/a2020_dm_term/DMApp/Planner/RestrictActivity.java
+++ app/src/main/java/com/example/a2020_dm_term/DMApp/Planner/RestrictActivity.java
@@ -1,6 +1,9 @@
 package com.example.a2020_dm_term.DMApp.Planner;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
@@ -23,12 +26,33 @@ public class RestrictActivity extends AppCompatActivity {
     Timer timer;
     String time;
     TimerTask TT;
+    IntentFilter intentFilter;
+
+    private static final String ScreenOff = "android.intent.action.SCREEN_OFF";
+    private static final String ScreenOn = "android.intent.action.SCREEN_ON";
+    ScreenOnReceiver screenOnReceiver;
+
     int elapsedTime = 0;
     int second;
     int minute;
     int hour;
+    int screenOn = 1;
     private final Handler handler = new Handler();
 
+    class ScreenOnReceiver extends BroadcastReceiver{
+        public void onReceive(Context context, Intent intent){
+            String action = intent.getAction();
+            Log.d("ScreenOnReceiver","ScreenOnReceiver, onReceive:"+action);
+            if(action.equals(Intent.ACTION_SCREEN_ON)){
+                screenOn = 1;
+                Log.d("ScreenOnReceiver","ScreenOnReceiver, ScreenOn");
+            }
+            else if (action.equals(Intent.ACTION_SCREEN_OFF)){
+                screenOn = 0;
+                Log.d("ScreenOnReceiver","ScreenOnReceiver, ScreenOFF");
+            }
+        }
+    }
     class RButtonListener implements View.OnClickListener{
         public void onClick(View v){
             Toast.makeText(getApplicationContext(), "방해금지 모드를 종료합니다.",Toast.LENGTH_SHORT).show();
@@ -53,7 +77,14 @@ public class RestrictActivity extends AppCompatActivity {
         button = (Button)findViewById(R.id.stopButton);
         RButtonListener ExitListener = new RButtonListener();
 
-        button.setOnClickListener(ExitListener);
+        screenOnReceiver = new ScreenOnReceiver();
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_SCREEN_ON);
+        intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
+        registerReceiver(screenOnReceiver,intentFilter);
+        //intent 설정들
+
+       button.setOnClickListener(ExitListener);
         timer = new Timer();
         /*아직까지 잘 모르겠는것 -> 화면 권한?
         onPause, onStop에서 어떻게든 해결해야 함.
@@ -76,15 +107,23 @@ public class RestrictActivity extends AppCompatActivity {
 
     @Override
     protected void onStop(){
-        TT.cancel();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if(screenOn == 1){
+                    Log.d("onStop","TT.cancel");
+                    TT.cancel();
+                }
+            }
+        }, 1000);
         super.onStop();
     }
-
+    /*
     @Override
     protected  void onPause(){
         TT.cancel();
         super.onPause();
-    }
+    }*/
 
     @Override
     protected void onRestart(){

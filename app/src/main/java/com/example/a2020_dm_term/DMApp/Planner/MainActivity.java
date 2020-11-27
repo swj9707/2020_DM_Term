04716296cef3cc9b711d048c752d4ae91678d456
amugae568 <anjffhgkwl00@gmail.com>
2020-11-28 03:47:28 +0900

    2020.11.28. - 변경사항 모든 버튼의 크기를 반응형으로 조절함 테이블의 이중 스크롤에서 가로 스크롤을 제거 전반적인 테마 변경 방해금지 모드에서 시간 포멧 변경

--- app/src/main/java/com/example/a2020_dm_term/DMApp/Planner/MainActivity.java
+++ app/src/main/java/com/example/a2020_dm_term/DMApp/Planner/MainActivity.java
@@ -4,19 +4,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
+import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 import com.example.a2020_dm_term.R;
 import com.example.a2020_dm_term.DMApp.DB.*;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ArrayList;
+import java.util.Locale;
 
 /*
 실제 플래너가 실행되는 공간.
@@ -30,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     Intent restrictIntent;
     Intent weeklyIntent;
     RelativeLayout timeTable;
+    NestedScrollView nestedScrollView;
 
     public static PlanDBController plnDBC;
     public static TaskDBController tskDBC;
@@ -39,8 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
     ArrayList<CustomTextView> taskList = new ArrayList<CustomTextView>();
 
-    int CELL_WIDTH;
-    int CELL_HEIGHT;
+    int CELL_SIZE;
 
     class weeklyButtonListener implements View.OnClickListener {
         public void onClick(View v) {
@@ -64,9 +65,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //레이아웃 불러오기
 
-        todayView =  (TextView)findViewById(R.id.today);
-        weeklyPlanButton = (Button)findViewById(R.id.weeklyPlanButton);
-        restrictModeButton = (Button)findViewById(R.id.restrictModeButton);
+        todayView = (TextView) findViewById(R.id.today);
+        weeklyPlanButton = (Button) findViewById(R.id.weeklyPlanButton);
+        restrictModeButton = (Button) findViewById(R.id.restrictModeButton);
         //xml 상에 있는 요소들 불러오기
 
         restrictIntent = new Intent(getApplicationContext(), RestrictActivity.class);
@@ -81,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
 
         date = new Date();
         //final String today = date.toString();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년MM월dd일");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 (E)", Locale.KOREAN);
         String today = sdf.format(date);
         todayView.setText(today);
 
@@ -117,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
         String time = Hr + ":" + Min + ":" + Sec;
         //그날 공부 총 지속 시간을 계산 해 내는 코드
         //방법은 간단하니 생략하도록 하겠음
-        Log.d("MainActivity","ContinuousTime : "+time);
+        Log.d("MainActivity", "ContinuousTime : " + time);
         //이 밑으로 형우꺼
         //시간표 생성
         timeTable = findViewById(R.id.main_time_table);
@@ -162,20 +163,28 @@ public class MainActivity extends AppCompatActivity {
         for (CustomTextView item : dummies)
             taskList.add(item);
 
-        CELL_WIDTH = (dp2px(390) - dp2px(20) - dp2px(1) * 7) / 7;
-        CELL_HEIGHT = dp2px(70);
+        Display display = getWindowManager().getDefaultDisplay();
+        int width = display.getWidth();
+        CELL_SIZE = (width - dp2px(1) * 7 - dp2px(10) * 2 - dp2px(20)) / 7;
+
+        int nested_width = width - dp2px(10) * 2;
+        int nested_height = (width - dp2px(10) * 2) / 8 * 7;
+        nestedScrollView = (NestedScrollView) findViewById(R.id.nested_scroll);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(nested_width, nested_height);
+        nestedScrollView.setLayoutParams(layoutParams);
 
         int idx = 0;
         for (String day : new String[]{"일", "월", "화", "수", "목", "금", "토"}) {
-            RelativeLayout.LayoutParams day_params = new RelativeLayout.LayoutParams(CELL_WIDTH, RelativeLayout.LayoutParams.WRAP_CONTENT);
-            day_params.setMargins(dp2px(1) * (idx + 1) + CELL_WIDTH * idx, 0, 0, 0);
+            RelativeLayout.LayoutParams day_params = new RelativeLayout.LayoutParams(CELL_SIZE, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            day_params.setMargins(dp2px(1) * (idx + 1) + CELL_SIZE * idx, 0, 0, 0);
             day_params.addRule(RelativeLayout.RIGHT_OF, R.id.blank);
             day_params.addRule(RelativeLayout.ALIGN_TOP, R.id.blank);
             TextView days = new TextView(this);
             days.setText(day);
-            days.setBackgroundColor(Color.WHITE);
+            days.setBackgroundColor(Color.parseColor("#3C3F41"));
             days.setLayoutParams(day_params);
             days.setGravity(Gravity.CENTER);
+            days.setTextColor(Color.parseColor("#BEBEBE"));
             timeTable.addView(days);
             idx++;
         }
@@ -184,10 +193,11 @@ public class MainActivity extends AppCompatActivity {
             TextView rowIndex = new TextView(this);
             rowIndex.setText(Integer.toString(hour - 1));
             rowIndex.setGravity(Gravity.CENTER);
-            rowIndex.setBackgroundColor(Color.WHITE);
+            rowIndex.setBackgroundColor(Color.parseColor("#3C3F41"));
             rowIndex.setId(hour * 10);
+            rowIndex.setTextColor(Color.parseColor("#BEBEBE"));
 
-            RelativeLayout.LayoutParams idx_params = new RelativeLayout.LayoutParams(dp2px(20), CELL_HEIGHT);
+            RelativeLayout.LayoutParams idx_params = new RelativeLayout.LayoutParams(dp2px(20), CELL_SIZE);
 
             idx_params.setMargins(0, dp2px(1), 0, 0);
             if (hour == 1)
@@ -199,16 +209,17 @@ public class MainActivity extends AppCompatActivity {
             timeTable.addView(rowIndex);
 
             for (int day = 1; day <= 7; day++) {
-                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(CELL_WIDTH, CELL_HEIGHT);
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(CELL_SIZE, CELL_SIZE);
                 params.addRule(RelativeLayout.RIGHT_OF, hour * 10);
                 params.addRule(RelativeLayout.ALIGN_TOP, hour * 10);
                 CustomTextView cell = new CustomTextView(this, 1);
-                params.setMargins(dp2px(1) * day + CELL_WIDTH * (day - 1), 0, 0, dp2px(1));
+                params.setMargins(dp2px(1) * day + CELL_SIZE * (day - 1), 0, 0, dp2px(1));
 
                 cell.setId(hour * 10 + day);
                 cell.setGravity(Gravity.CENTER);
-                cell.setBackgroundColor(Color.WHITE);
+                cell.setBackgroundColor(Color.parseColor("#505355"));
                 cell.setLayoutParams(params);
+                cell.setTextColor(Color.parseColor("#BEBEBE"));
 
                 timeTable.addView(cell);
             }
@@ -224,13 +235,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void mergeCells(int period, int id) {
-        System.out.println(id);
         CustomTextView cell = (CustomTextView) findViewById(id);
         for (int i = 1; i <= period - 1; i++)
             timeTable.removeView(findViewById(id + i * 10));
 
         RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) cell.getLayoutParams();
-        params.height = period * CELL_HEIGHT + (period - 1) * dp2px(1);
+        params.height = period * CELL_SIZE + (period - 1) * dp2px(1);
         cell.setLayoutParams(params);
     }
 
--- app/src/main/java/com/example/a2020_dm_term/DMApp/Planner/WeeklyPlannerActivity.java
+++ app/src/main/java/com/example/a2020_dm_term/DMApp/Planner/WeeklyPlannerActivity.java
@@ -11,6 +11,7 @@ import android.graphics.Point;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.DragEvent;
 import android.view.Gravity;
 import android.view.View;
@@ -30,8 +31,8 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
     ArrayList<CustomTextView> taskList = new ArrayList<CustomTextView>();//배치된 작업 목록
     ArrayList<CustomTextView> blockList = new ArrayList<CustomTextView>();//생성된 블럭 목록
 
-    int CELL_WIDTH;
-    int CELL_HEIGHT;
+    int CELL_SIZE;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +40,10 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_weeklyplan);
 
         //시간표 셀 크기
-        CELL_WIDTH = dp2px(70);
-        CELL_HEIGHT = dp2px(70);
+        Display display = getWindowManager().getDefaultDisplay();
+        int width = display.getWidth();
+        CELL_SIZE = (width - dp2px(1) * 7 - dp2px(10) * 2 - dp2px(20)) / 7;
+
 
         context = this;
         task_layout = (LinearLayout) findViewById(R.id.task_layout);
@@ -54,16 +57,32 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
         주의할 점은 (1,1)부터 시작되기 때문에 원하는 시간과 요일 값에 1을 더해줘야할 필요가 있음
         timeTable의 배경을 검정으로 설정하고 셀들의 마진을 1dp씩 설정해서 테두리를 표시하고 셀들은 흰색으로 설정
         */
+        int idx = 0;
+        for (String day : new String[]{"일", "월", "화", "수", "목", "금", "토"}) {
+            RelativeLayout.LayoutParams day_params = new RelativeLayout.LayoutParams(CELL_SIZE, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            day_params.setMargins(dp2px(1) * (idx + 1) + CELL_SIZE * idx, 0, 0, 0);
+            day_params.addRule(RelativeLayout.RIGHT_OF, R.id.blank);
+            day_params.addRule(RelativeLayout.ALIGN_TOP, R.id.blank);
+            TextView days = new TextView(this);
+            days.setText(day);
+            days.setBackgroundColor(Color.parseColor("#3C3F41"));
+            days.setTextColor(Color.parseColor("#BEBEBE"));
+            days.setLayoutParams(day_params);
+            days.setGravity(Gravity.CENTER);
+            timeTable.addView(days);
+            idx++;
+        }
 
         for (int hour = 1; hour <= 24; hour++) {
             //행의 가장 첫 셀 시간을 나타내는 인덱스 생성
             TextView rowIndex = new TextView(this);
             rowIndex.setText(Integer.toString(hour - 1));
             rowIndex.setGravity(Gravity.CENTER);
-            rowIndex.setBackgroundColor(Color.WHITE);
+            rowIndex.setBackgroundColor(Color.parseColor("#3C3F41"));
+            rowIndex.setTextColor(Color.parseColor("#BEBEBE"));
             rowIndex.setId(hour * 10);
 
-            RelativeLayout.LayoutParams idx_params = new RelativeLayout.LayoutParams(dp2px(20), CELL_HEIGHT);
+            RelativeLayout.LayoutParams idx_params = new RelativeLayout.LayoutParams(dp2px(20), CELL_SIZE);
             idx_params.setMargins(0, dp2px(1), 0, 0);
             if (hour == 1)//좌측 상단의 빈 셀을 기준으로 차례로 아래에 연결한다.
                 idx_params.addRule(RelativeLayout.BELOW, R.id.blank);
@@ -74,16 +93,17 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
             timeTable.addView(rowIndex);
 
             for (int day = 1; day <= 7; day++) {
-                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(CELL_WIDTH, CELL_HEIGHT);
+                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(CELL_SIZE, CELL_SIZE);
                 params.addRule(RelativeLayout.RIGHT_OF, hour * 10);
                 params.addRule(RelativeLayout.ALIGN_TOP, hour * 10);
                 CustomTextView cell = new CustomTextView(this, TABLE_CELL);
-                params.setMargins(dp2px(1) * day + CELL_WIDTH * (day - 1), 0, 0, dp2px(1));
+                params.setMargins(dp2px(1) * day + CELL_SIZE * (day - 1), 0, 0, dp2px(1));
 
                 cell.setId(hour * 10 + day);
                 cell.setOnDragListener(new myOnDragListener());//timeTable 위에서 다른 일정으로 배치할 수 있도록 드래스 리스너 등록
                 cell.setGravity(Gravity.CENTER);
-                cell.setBackgroundColor(Color.WHITE);
+                cell.setBackgroundColor(Color.parseColor("#505355"));
+                cell.setTextColor(Color.parseColor("#BEBEBE"));
                 cell.setLayoutParams(params);
 
                 timeTable.addView(cell);
@@ -130,7 +150,7 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
             public void onClick(View view) {
                 System.out.println("confirm button is clicked.");
                 task.title = task_name.getText().toString();
-                if (task.title == null) {//제목 설정하지 않으면 무시
+                if (task.title.equals("")) {//제목 설정하지 않으면 무시
                     Toast.makeText(getApplicationContext(), "제목을 설정해주세요.",Toast.LENGTH_SHORT).show();
                     return;
                 }
@@ -144,6 +164,7 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
                 newBtn.setText(task.title + "\n" + task.period);
                 newBtn.setGravity(Gravity.CENTER);
                 newBtn.setBackgroundDrawable(getResources().getDrawable(R.drawable.edge));
+                newBtn.setTextColor(Color.parseColor("#BEBEBE"));
                 newBtn.setLayoutParams(params);
 
                 setDrag(newBtn);//배치된 일정을 드래그해서 다른 시간에 배치할 수 있도록 롱클릭 이벤트 설정
@@ -182,7 +203,7 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
                                 taskList.remove((CustomTextView) v);
                                 //설정된 값들을 빈 값으로 변경하고 할당된 이벤트들을 모두 삭제함
                                 customView.task = null;
-                                customView.droppable = true;
+                                customView.droppable = 1;
                                 customView.setOnClickListener(null);
                                 customView.setOnLongClickListener(null);
                                 break;
@@ -244,7 +265,7 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
 
                     for (int i = 0; i <= droppedCell.task.period - 1; i++) {//배치된 작업이 다른 일정을 겹치는지 검사
                         CustomTextView belowCell = (CustomTextView) findViewById(view.getId() + i * 10);
-                        if (!belowCell.droppable) {
+                        if (belowCell.droppable == 0) {
                             Toast.makeText(getApplicationContext(), "다른 일정과 겹칩니다.", Toast.LENGTH_SHORT).show();
                             ext = true;
                             break;
@@ -255,7 +276,7 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
 
                     //드롭되었을 때 목표 셀에 정보 설정하고 이벤트 등록
                     targetCell.setText(droppedCell.task.title);
-                    targetCell.droppable = false;
+                    targetCell.droppable = 0;
                     targetCell.task = droppedCell.task;
                     targetCell.task.hour = row;
                     targetCell.task.day = column;
@@ -266,7 +287,7 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
                     if (droppedCell.type == TABLE_CELL) {
                         splitCell(droppedCell.task.period, droppedCell.getId());
                         droppedCell.task = null;
-                        droppedCell.droppable = true;
+                        droppedCell.droppable = 1;
                         droppedCell.setOnLongClickListener(null);
                         droppedCell.setOnClickListener(null);
                     }
@@ -300,15 +321,15 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
 
     public void splitCell(int period, int id) {//병합된 셀을 다시 분리하고 데이터를 초기화
         CustomTextView currentCell = (CustomTextView) findViewById(id);//분할되지 않은 셀
-        currentCell.droppable = true;
+        currentCell.droppable = 1;
         currentCell.setText("");
         //설정 초기화
 
         //셀 폭, 높이 설정. 최좌측의 인덱스 셀을 기준으로 상단, 우측 배열
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(CELL_WIDTH, CELL_HEIGHT);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(CELL_SIZE, CELL_SIZE);
         params.addRule(RelativeLayout.RIGHT_OF, id / 10 * 10);
         params.addRule(RelativeLayout.ALIGN_TOP, id / 10 * 10);
-        params.setMargins(dp2px(1) * (id % 10) + CELL_WIDTH * ((id % 10) - 1), 0, 0, dp2px(1));//마진 값 설정
+        params.setMargins(dp2px(1) * (id % 10) + CELL_SIZE * ((id % 10) - 1), 0, 0, dp2px(1));//마진 값 설정
 
         currentCell.setLayoutParams(params);
         
@@ -317,15 +338,16 @@ public class WeeklyPlannerActivity extends AppCompatActivity {
             int newId = id + (10 * i);
             newCell.setId(newId);
 
-            params = new RelativeLayout.LayoutParams(CELL_WIDTH, CELL_HEIGHT);
-            params.setMargins(dp2px(1) * (id % 10) + CELL_WIDTH * ((id % 10) - 1), 0, 0, dp2px(1));
+            params = new RelativeLayout.LayoutParams(CELL_SIZE, CELL_SIZE);
+            params.setMargins(dp2px(1) * (id % 10) + CELL_SIZE * ((id % 10) - 1), 0, 0, dp2px(1));
             int targetId = newId / 10 * 10;//최좌측 인덱스의 아이디
             params.addRule(RelativeLayout.RIGHT_OF, targetId);
             params.addRule(RelativeLayout.ALIGN_TOP, targetId);
 
             newCell.setOnDragListener(new myOnDragListener());//드래그를 받을 수 있는 리스너 등록
             newCell.setGravity(Gravity.CENTER);
-            newCell.setBackgroundColor(Color.WHITE);
+            newCell.setBackgroundColor(Color.parseColor("#505355"));
+            newCell.setTextColor(Color.parseColor("#BEBEBE"));
             newCell.setLayoutParams(params);
 
             timeTable.addView(newCell);
